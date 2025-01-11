185bb9d144db580ceb97cd3f169c6d7007d10bb8
Arup Basak <email.arupbasak@gmail.com>
2025-01-11 15:55:56 +0530

    changed params in use server

--- utils/insertValues.js
+++ utils/insertValues.js
@@ -1,11 +1,13 @@
-// "use server"
+"use server"
 
 import { v4 as uuidv4 } from "uuid";
 import db from "@/utils/db";
 
-export const insertValues = async (values) => {
+export const insertValues = async (jobPosition, jobDescription, yearsOfExperience) => {
     const resp = await db.insert(AiInterview).values({
         id: uuidv4(),
-        ...values,
+        jobPosition, 
+        jobDescription, 
+        yearsOfExperience
       });
 }
